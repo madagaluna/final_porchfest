79fa0f56dbec138ec0a3f5bb673122ff43f982ff
Mary B. <mbradley@hbs.edu>
2026-05-14 11:27:43 -0400

    deploy updated version

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/final_porchfest.2"
+  base: "/final_porchfest"
 })
